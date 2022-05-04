4b94719ef0bc35829f001a50672ce7640a1eed49
diego <diegozaratean@gmail.com>
2022-05-04 12:01:12 -0500

    add item list container

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,17 @@
 import Alert from 'react-bootstrap/Alert'
+import ItemCount from '../ItemCount/ItemCount'
 
 export default function ItemListContainer({greeting}){
+    const addTocart = (count) =>{
+        console.log(`add to cart ${count} items`);
+    }
     return (
         <>           
             <Alert key='success' variant='success'>
                 {greeting}
             </Alert>
-        </>        
+            <ItemCount stock='5' initial='1' onAdd={addTocart}/>
+        </>   
+
     )
 }
