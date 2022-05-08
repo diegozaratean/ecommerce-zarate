5ef9a45a6346601ade415633e26bf3b369815b5c
diego <diegozaratean@gmail.com>
2022-05-08 13:38:32 -0500

    add component ImteList and Item and fill products with promise

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,21 @@
 import Alert from 'react-bootstrap/Alert'
 import ItemCount from '../ItemCount/ItemCount'
 import { toast } from "react-toastify";
+import {products} from "../../data/products"
+import ItemList from '../ItemList/ItemList';
 
 export default function ItemListContainer({greeting}){
+
+    const getProductsTask = new Promise((resolve,reject) =>{
+        setTimeout(() =>{
+            resolve(products);
+        },3000)
+    })
+
+    getProductsTask.then((result) =>{
+        console.log(result);
+    })
+
     const addTocart = (count) =>{
         console.log(`add to cart ${count} items`);
         toast.success(`add to cart ${count} items!`, {
@@ -20,7 +33,8 @@ export default function ItemListContainer({greeting}){
             <Alert key='success' variant='success'>
                 {greeting}
             </Alert>
-            <ItemCount stock='5' initial='1' onAdd={addTocart}/>
+            {/* <ItemCount stock='5' initial='1' onAdd={addTocart}/> */}
+            <ItemList items = {products} onAdd={addTocart}/>
         </>   
 
     )
